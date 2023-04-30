94e5ca5734d8c1564367ffe5f2e28aa7be825d8a
Alejandro Román Caballero <32822670+Alejandro314@users.noreply.github.com>
2023-04-30 11:40:46 +0200

    Arreglado editar coche

--- CarUPO/resources/views/editarCoche.blade.php
+++ CarUPO/resources/views/editarCoche.blade.php
@@ -77,7 +77,8 @@
         <label for="categorias" class="form-label">{{ __('messages.categorias') }}</label>
         <div class="containerP">
             @foreach (DB::table('categorias')->get() as $categoria)
-            @if (DB::table('producto_categorias')->where('fk_producto_id', '=', $coche->fk_producto_id)->where('fk_categoria_id', '=', $categoria->id)->exists())
+            @if(DB::table('producto_categorias')->where('fk_producto_id', '=', $coche->fk_producto_id)->where('fk_categoria_id', '=', $categoria->id)
+            ->exists())
             <div class="form-check">
                 <input class="form-check-input" name="categorias[]" type="checkbox" value="{{ $categoria->id }}" id="flexCheckChecked" checked>
                 <label class="form-check-label" for="flexCheckChecked">
@@ -91,14 +92,7 @@
                     {{ $categoria->nombre }}
                 </label>
             </div>
-            <label for="foto" class="form-label">{{ __('messages.foto') }}</label>
-            <input type="file" name="foto" class="form-control mb-2">
-            @if ($errors->has('foto'))
-                <div class="alert alert-danger">
-                    {{ $errors->first('foto') }}
-                </div>
             @endif
-
             @endforeach
         </div>
         <label for="foto" class="form-label">{{ __('messages.foto') }}</label>
@@ -115,6 +109,8 @@
             {{ $errors->first('precio') }}
         </div>
         @endif
+
+
         <div class="justify-content-center d-flex">
             <input type="hidden" name="id" value="{{ $coche->id }}">
             <input type="hidden" name="idp" value="{{ $coche->producto->id }}">
@@ -129,16 +125,8 @@
             <button class="btn btn-danger btn-block" type="submit">
                 {{ __('messages.atras') }}
             </button>
-
         </form>
-        <div class="d-flex justify-content-start mt-5">
-            <form action="{{ route('mostrarProductos') }}" method="GET">
-                @csrf
-                <button class="btn btn-danger btn-block" type="submit">
-                    {{ __('messages.atras') }}
-                </button>
-            </form>
-        </div>
     </div>
+</div>
 
 @endsection
