61235f2d99d806b3d133a6224af37365f47dd7ec
lganpin <lidiagantp@gmail.com>
2023-05-23 09:51:06 +0200

    Cambiada barra de navegación

--- CarUPO/resources/views/plantilla.blade.php
+++ CarUPO/resources/views/plantilla.blade.php
@@ -44,9 +44,6 @@
                         {{ app()->setLocale('en') }}
                         @endif
                         <li class="nav-item justify-content-center d-flex">
-                            <a class="nav-link text-white" href="{{route('miPerfil')}}">{{ __('messages.perfil') }}</a>
-                        </li>
-                        <li class="nav-item justify-content-center d-flex">
                             <a class="nav-link text-white" href="{{route('mostrarProductos')}}">{{ __('messages.productos') }}</a>
                         </li>
 
@@ -66,22 +63,28 @@
                         @else
                         <li class="nav-item justify-content-center d-flex">
                             <a class="nav-link text-white" href="{{route('misCompras')}}">{{ __('messages.misCompras') }}</a>
-                        </li>
-                        <li class="nav-item justify-content-center d-flex">
-                            <a class="nav-link text-white" href="{{route('mostrarCarrito')}}">{{ __('messages.carrito') }}</a>
-                        </li>
+                        </li>                        
                         <li class="nav-item justify-content-center d-flex">
                             <a class="nav-link text-white" href="{{ route('misFavoritos') }}">{{ __('messages.misFavs') }}</a>
-
+                        </li>
+                        <li class="nav-item justify-content-center d-flex">
+                            <a class="nav-link text-white" href="{{route('mostrarCarrito')}}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
+                                </svg>
+                            </a>
                         </li>
                         @endif
                     </ul>
                 </div>
-                <div class="dropdown justify-content-center d-flex">
+                <div class="dropdown justify-content-center d-flex mx-4">
                     <button class="buttonP btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                         {{ Auth::user()->name }}
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li>
+                            <a class="dropdown-item" href="{{route('miPerfil')}}">{{ __('messages.perfil') }}</a>
+                        </li>
                         <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('messages.logout') }}</a>
                         </li>
                     </ul>
